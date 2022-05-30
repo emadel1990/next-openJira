f0181d9b59e09d85b3d94003c9871ca21c2ec6d3
Jose Emanuel Delgado <emadevjs.nbc@gmail.com>
2022-05-29 22:24:30 -0300

    fin de seccion 8

--- context/entries/entriesReducer.ts
+++ context/entries/entriesReducer.ts
@@ -11,12 +11,12 @@ export const entriesReducer = (state: EntriesState, action: EntriesActionType): 
     case '[Entry] - Add-Entry':
       return {
         ...state,
-        entries: [...state.entries, action.payload]
+        entries: [...state.entries!, action.payload]
       };
     case '[Entry] - Update-Entry':
       return {
         ...state,
-        entries: state.entries.map(entry => {
+        entries: state.entries!.map(entry => {
           if (entry._id === action.payload._id) {
             entry.status = action.payload.status;
             entry.description = action.payload.description;
